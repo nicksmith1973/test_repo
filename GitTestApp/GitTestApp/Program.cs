36422b474fbf24e4d14861b0c08a3764f2ed09a5
NickSmith1973 <nick.smith@inmarsat.com>
2018-10-09 20:36:41 +0200

    A new project

---
+++ GitTestApp/GitTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace GitTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true) { Thread.Sleep(1000); }
+        }
+    }
+}
